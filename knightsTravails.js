5e22dc6e9e57ed6053f19d180d2750ae2e294a33
Joe <caspar0912@googlemail.com>
2022-11-01 14:09:23 +0000

    add basic functions for gameboard logic

--- knightsTravails.js
+++ knightsTravails.js
@@ -12,3 +12,46 @@
  6- After thinking through my logic I'm not sure I'll even need to create a matrix. if I can
  have a starting position (i.e [x0,y0]) and an end position (say [x4y4]) then I
  only really need to create a queue and use the offset logic to work out depth.*/
+
+class Node {
+  constructor([x, y], parent = null) {
+    this.location = [x, y];
+    this.parent = parent;
+  }
+}
+/* create a board with cells containing a [false] value */
+const createGameBoard = () => {
+  const gameBoard = [];
+  for (let i = 0; i <= 7; i++) {
+    gameBoard[i] = [];
+    for (let j = 0; j <= 7; j++) {
+      gameBoard[i][j] = false;
+    }
+  }
+  return gameBoard;
+};
+
+/* make sure the tile is empty [false] */
+const checkTileOpen = (board, loc) => {
+  const x = loc[0];
+  const y = loc[1];
+  return !board[x][y];
+};
+
+/* make sure the suggested move is with 0-7x 0-7y bounds */
+const isWithinBounds = (loc) => {
+  if (loc[0] < 0 || loc[0] > 7 || loc[1] < 0 || loc[1] > 7) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const availableMoves = (loc) => {};
+
+const board = createGameBoard();
+console.log(board);
+const tile = new Node([7, 3]);
+console.log(tile.location[0]);
+console.log(isWithinBounds(tile.location));
+console.log(checkTileOpen(board, tile.location));
